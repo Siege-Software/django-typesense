8868381e69637ec8098f8a36896be2cd34d4f9b9
EricOuma <odhiamboeric0049@gmail.com>
2023-08-28 19:13:37 +0300

    Live Search initial implementation

--- django_typesense/admin.py
+++ django_typesense/admin.py
@@ -1,7 +1,19 @@
 import logging
 
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin import helpers
+from django.contrib.admin.options import IncorrectLookupParameters
+from django.contrib.admin.utils import model_ngettext
+from django.contrib.auth.admin import csrf_protect_m
+from django.core.exceptions import PermissionDenied
+from django.db import transaction, router
 from django.forms import forms
+from django.http import JsonResponse, HttpResponseRedirect
+from django.template.loader import render_to_string
+from django.template.response import TemplateResponse, SimpleTemplateResponse
+from django.utils.translation import gettext as _
+from django.utils.translation import ngettext
+
 from django_typesense.methods import typesense_search
 from django_typesense.paginator import TypesenseSearchPaginator
 
@@ -12,26 +24,214 @@ BOOLEAN_TRUES = ["y", "Y", "yes", "1", 1, True, "True", "true"]
 
 
 class TypesenseSearchAdminMixin(admin.ModelAdmin):
-
     @property
     def media(self):
         super_media = super().media
         return forms.Media(
-            js=super_media._js + ["admin/js/django_typesense-typesense.js"],
+            js=super_media._js + ["admin/js/search-typesense.js"],
             css=super_media._css,
         )
 
+    @csrf_protect_m
+    def changelist_view(self, request, extra_context=None):
+        """
+        The 'change list' admin view for this model.
+        """
+        from django.contrib.admin.views.main import ERROR_FLAG
+
+        app_label = self.opts.app_label
+        if not self.has_view_or_change_permission(request):
+            raise PermissionDenied
+
+        try:
+            cl = self.get_changelist_instance(request)
+        except IncorrectLookupParameters:
+            # Wacky lookup parameters were given, so redirect to the main
+            # changelist page, without parameters, and pass an 'invalid=1'
+            # parameter via the query string. If wacky parameters were given
+            # and the 'invalid=1' parameter was already in the query string,
+            # something is screwed up with the database, so display an error
+            # page.
+            if ERROR_FLAG in request.GET:
+                return SimpleTemplateResponse(
+                    "admin/invalid_setup.html",
+                    {
+                        "title": _("Database error"),
+                    },
+                )
+            return HttpResponseRedirect(request.path + "?" + ERROR_FLAG + "=1")
+
+        # If the request was POSTed, this might be a bulk action or a bulk
+        # edit. Try to look up an action or confirmation first, but if this
+        # isn't an action the POST will fall through to the bulk edit check,
+        # below.
+        action_failed = False
+        selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+
+        actions = self.get_actions(request)
+        # Actions with no confirmation
+        if (
+            actions
+            and request.method == "POST"
+            and "index" in request.POST
+            and "_save" not in request.POST
+        ):
+            if selected:
+                response = self.response_action(
+                    request, queryset=cl.get_queryset(request)
+                )
+                if response:
+                    return response
+                else:
+                    action_failed = True
+            else:
+                msg = _(
+                    "Items must be selected in order to perform "
+                    "actions on them. No items have been changed."
+                )
+                self.message_user(request, msg, messages.WARNING)
+                action_failed = True
+
+        # Actions with confirmation
+        if (
+            actions
+            and request.method == "POST"
+            and helpers.ACTION_CHECKBOX_NAME in request.POST
+            and "index" not in request.POST
+            and "_save" not in request.POST
+        ):
+            if selected:
+                response = self.response_action(
+                    request, queryset=cl.get_queryset(request)
+                )
+                if response:
+                    return response
+                else:
+                    action_failed = True
+
+        if action_failed:
+            # Redirect back to the changelist page to avoid resubmitting the
+            # form if the user refreshes the browser or uses the "No, take
+            # me back" button on the action confirmation page.
+            return HttpResponseRedirect(request.get_full_path())
+
+        # If we're allowing changelist editing, we need to construct a formset
+        # for the changelist given all the fields to be edited. Then we'll
+        # use the formset to validate/process POSTed data.
+        formset = cl.formset = None
+
+        # Handle POSTed bulk-edit data.
+        if request.method == "POST" and cl.list_editable and "_save" in request.POST:
+            if not self.has_change_permission(request):
+                raise PermissionDenied
+            FormSet = self.get_changelist_formset(request)
+            modified_objects = self._get_list_editable_queryset(
+                request, FormSet.get_default_prefix()
+            )
+            formset = cl.formset = FormSet(
+                request.POST, request.FILES, queryset=modified_objects
+            )
+            if formset.is_valid():
+                changecount = 0
+                with transaction.atomic(using=router.db_for_write(self.model)):
+                    for form in formset.forms:
+                        if form.has_changed():
+                            obj = self.save_form(request, form, change=True)
+                            self.save_model(request, obj, form, change=True)
+                            self.save_related(request, form, formsets=[], change=True)
+                            change_msg = self.construct_change_message(
+                                request, form, None
+                            )
+                            self.log_change(request, obj, change_msg)
+                            changecount += 1
+                if changecount:
+                    msg = ngettext(
+                        "%(count)s %(name)s was changed successfully.",
+                        "%(count)s %(name)s were changed successfully.",
+                        changecount,
+                    ) % {
+                        "count": changecount,
+                        "name": model_ngettext(self.opts, changecount),
+                    }
+                    self.message_user(request, msg, messages.SUCCESS)
+
+                return HttpResponseRedirect(request.get_full_path())
+
+        # Handle GET -- construct a formset for display.
+        elif cl.list_editable and self.has_change_permission(request):
+            FormSet = self.get_changelist_formset(request)
+            formset = cl.formset = FormSet(queryset=cl.result_list)
+
+        # Build the list of media to be used by the formset.
+        if formset:
+            media = self.media + formset.media
+        else:
+            media = self.media
+
+        # Build the action form and populate it with available actions.
+        if actions:
+            action_form = self.action_form(auto_id=None)
+            action_form.fields["action"].choices = self.get_action_choices(request)
+            media += action_form.media
+        else:
+            action_form = None
+
+        selection_note_all = ngettext(
+            "%(total_count)s selected", "All %(total_count)s selected", cl.result_count
+        )
+
+        context = {
+            **self.admin_site.each_context(request),
+            "module_name": str(self.opts.verbose_name_plural),
+            "selection_note": _("0 of %(cnt)s selected") % {"cnt": len(cl.result_list)},
+            "selection_note_all": selection_note_all % {"total_count": cl.result_count},
+            "title": cl.title,
+            "subtitle": None,
+            "is_popup": cl.is_popup,
+            "to_field": cl.to_field,
+            "cl": cl,
+            "media": media,
+            "has_add_permission": self.has_add_permission(request),
+            "opts": cl.opts,
+            "action_form": action_form,
+            "actions_on_top": self.actions_on_top,
+            "actions_on_bottom": self.actions_on_bottom,
+            "actions_selection_counter": self.actions_selection_counter,
+            "preserved_filters": self.get_preserved_filters(request),
+            **(extra_context or {}),
+        }
+
+        request.current_app = self.admin_site.name
+        template = self.change_list_template or [
+            "admin/%s/%s/change_list.html" % (app_label, self.opts.model_name),
+            "admin/%s/change_list.html" % app_label,
+            "admin/change_list.html",
+        ]
+        template_response = TemplateResponse(request, template, context)
+
+        is_ajax = request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+        if is_ajax:
+            html = template_response.render().rendered_content
+            return JsonResponse(data={"html": html}, safe=False)
+
+        return template_response
+
     def get_sortable_by(self, request):
         sortable_fields = super().get_sortable_by(request)
+
         # Remove fields that sort has not been enabled in typesense schema
         def is_sortable(values):
-            default_sortable_data_types = {'int32', 'int64', 'float', 'bool'}
-            if values['type'] in default_sortable_data_types:
-                return values.get('sort', True)
+            default_sortable_data_types = {"int32", "int64", "float", "bool"}
+            if values["type"] in default_sortable_data_types:
+                return values.get("sort", True)
 
-            return values.get('sort', False)
+            return values.get("sort", False)
 
-        typesense_sortable_fields = {key for key, values in self.model.typesense_fields.items() if is_sortable(values)}
+        typesense_sortable_fields = {
+            key
+            for key, values in self.model.typesense_fields.items()
+            if is_sortable(values)
+        }
         return set(sortable_fields).intersection(typesense_sortable_fields)
 
     def get_results(self, request):
@@ -67,6 +267,6 @@ class TypesenseSearchAdminMixin(admin.ModelAdmin):
             page=page_num,
             per_page=self.list_per_page,
             filter_by=filter_by,
-            sort_by=sort_by
+            sort_by=sort_by,
         )
         return results
--- django_typesense/static/admin/js/search-typesense.js
+++ django_typesense/static/admin/js/search-typesense.js
@@ -1,55 +1,37 @@
 (function($) {
     $(document).ready(function () {
-        const user_input = $("#searchbar");
-        const post_user_input = $("#post_search_term");
-        const delegates_div = $('#changelist-form');
-        const endpoint = JSON.parse(document.getElementById('search_typesense').textContent);
-        const delay_by_in_ms = 100;
-        let scheduled_function = false;
-        const urlParams = new URLSearchParams(window.location.search);
-
-        function get_query(){
-            var url = document.location.href;
-            var qs = url.substring(url.indexOf('?') + 1).split('&');
-            for(var i = 0, result = {}; i < qs.length; i++){
-                qs[i] = qs[i].split('=');
-                if (qs[i][1]) {
-                    result[qs[i][0]] = decodeURIComponent(qs[i][1]);
-                }
-            }
-            return result;
-        }
-
         let ajax_call = function (endpoint, request_parameters) {
             $.getJSON(endpoint, request_parameters)
                 .done(response => {
-                    // fade out the delegates_div, then:
-                    delegates_div.fadeTo('fast', 0.5).promise().then(() => {
-                        // replace the HTML contents
-                        delegates_div.html(response['html_from_view']);
-                        // fade-in the div with new contents
-                        delegates_div.fadeTo('fast', 1);
-                        post_user_input.val(user_input.val());
+                    changelist_form.fadeTo('fast', 0.5).promise().then(() => {
+                        changelist_form.html($(response['html']).find('#changelist-form').html());
+                        changelist_form.fadeTo('fast', 1);
+                        // searchbar.val(user_input.val());
                     })
+                })
+                .fail(function( jqxhr, textStatus, error ) {
+                    var err = textStatus + ", " + error;
+                    console.log( "Request Failed: " + err );
+                    changelist_form.html('Sorry. An error occurred on our end.');
                 });
         };
 
-        ajax_call(endpoint, Object.assign({search_query:''}, get_query()))
+        const searchbar = $("#searchbar");
+        const changelist_form = $('#changelist-form');
+        const delay_by_in_ms = 0;
+        let scheduled_function = false;
+        let endpoint = window.location.origin + window.location.pathname
 
-        user_input.on('keyup', function () {
-            let user_input_val = $(this).val();
-            const search_parameters = {
-                search_query:user_input_val, // value of user_input: the HTML element with ID user-input
-            };
+        searchbar.on('input', function () {
+            let search_value = $(this).val().trim();
+            const search_parameters = {q:search_value};
+            const urlParams = new URLSearchParams(window.location.search);
+            const request_parameters = Object.assign(search_parameters, urlParams);
 
-            const request_parameters = Object.assign(search_parameters, get_query());
-
-            delegates_div.html('<h1>Searching...</h1>');
-            // if scheduled_function is NOT false, cancel the execution of the function
+            changelist_form.html('<h1>Searching...</h1>');
             if (scheduled_function) {
                 clearTimeout(scheduled_function);
             }
-            // setTimeout returns the ID of the function to be executed
             scheduled_function = setTimeout(ajax_call, delay_by_in_ms, endpoint, request_parameters);
         })
     });
