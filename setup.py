d821f89afb9a8c90a70288f6beb1195b5c532c9e
EricOuma <odhiamboeric0049@gmail.com>
2023-09-11 02:39:25 +0300

    update patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="django_typesense",
     author="Siege Software",
     author_email="info@siege.ai",
-    version="0.1.1-alpha",
+    version="0.1.1",
     install_requires=[
         "django",
         "typesense",
