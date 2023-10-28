4e861c1f07f471c7cc29b49dcea2a7c42d74e6c3
KengoWada <louiskengo3@gmail.com>
2023-10-28 12:28:17 +0300

    Added django test models and collections.

---
+++ tests/collections.py
@@ -0,0 +1,18 @@
+from django_typesense import fields
+from django_typesense.collections import TypesenseCollection
+
+
+class SongCollection(TypesenseCollection):
+    query_by_fields = "title,artist_names,genre_name"
+
+    title = fields.TypesenseCharField()
+    genre_name = fields.TypesenseCharField(value="genre.name")
+    genre_id = fields.TypesenseSmallIntegerField()
+    release_date = fields.TypesenseDateField(
+        value="release_date_timestamp", optional=True
+    )
+    artist_names = fields.TypesenseArrayField(
+        base_field=fields.TypesenseCharField(), value="artist_names"
+    )
+    number_of_comments = fields.TypesenseSmallIntegerField(index=False, optional=True)
+    number_of_views = fields.TypesenseSmallIntegerField(index=False, optional=True)
