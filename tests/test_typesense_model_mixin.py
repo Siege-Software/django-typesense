0fdd1b972d9faeddd3ec1148bb9fee31fc0f5a38
KengoWada <louiskengo3@gmail.com>
2023-11-02 22:32:59 +0300

    #26 Updated to use utils get_document.

--- tests/test_typesense_model_mixin.py
+++ tests/test_typesense_model_mixin.py
@@ -1,12 +1,11 @@
 from datetime import date, timedelta
 
 from django.test import TestCase
-from typesense import exceptions
 
-from django_typesense.typesense_client import client
 from django_typesense.mixins import TypesenseManager
 
 from tests.models import Artist, Genre, Song
+from tests.utils import get_document
 
 
 class TestTypeSenseMixin(TestCase):
@@ -22,12 +21,6 @@ class TestTypeSenseMixin(TestCase):
         )
         self.song.artists.add(self.artist.pk)
 
-    def get_document(self, schema_name, document_id):
-        try:
-            return client.collections[schema_name].documents[document_id].retrieve()
-        except exceptions.ObjectNotFound:
-            return None
-
     def test_get_collection_class(self):
         collection_class = Song.get_collection_class()
         self.assertEqual(collection_class.__name__, "SongCollection")
@@ -37,28 +30,28 @@ class TestTypeSenseMixin(TestCase):
 
     def test_update_collection(self):
         schema_name = self.song.collection_class.schema_name
-        song_document = self.get_document(schema_name, self.song.pk)
+        song_document = get_document(schema_name, self.song.pk)
         self.assertEqual(song_document["genre_name"], self.song.genre.name)
 
         genre_name = "Dancehall"
         self.genre.name = genre_name
         self.genre.save(update_fields=["name"])
 
-        song_document = self.get_document(schema_name, self.song.pk)
+        song_document = get_document(schema_name, self.song.pk)
         self.assertNotEqual(song_document["genre_name"], self.song.genre.name)
         self.assertEqual(self.song.genre.name, genre_name)
 
         Song.objects.get_queryset().update()
-        song_document = self.get_document(schema_name, self.song.pk)
+        song_document = get_document(schema_name, self.song.pk)
         self.assertEqual(song_document["genre_name"], genre_name)
         self.assertEqual(song_document["genre_name"], self.song.genre.name)
 
     def test_delete_collection(self):
         schema_name = self.song.collection_class.schema_name
-        song_document = self.get_document(schema_name, self.song.pk)
+        song_document = get_document(schema_name, self.song.pk)
         self.assertEqual(song_document["title"], self.song.title)
 
         Song.objects.get_queryset().delete()
 
-        song_document = self.get_document(schema_name, self.song.pk)
+        song_document = get_document(schema_name, self.song.pk)
         self.assertIsNone(song_document)
